c09f1584cafe316bb5dfb106c22ead56d6bc4fa5
Your Name <you@example.com>
2022-07-17 13:07:35 +0300

    rename

--- src/Mile.java
+++ src/Mile.java
@@ -1,13 +1,13 @@
-public class MilesCalculation {
+public class Mile {
 
     public static void main(String[] args) {
 
         int price = 340; // Цена билета
         int ruble = 20; // одна миля за каждые 20 рублей
-        int x = (price/ruble);
+        int bonus = (price / ruble);
 
         System.out.println("За купленный билет вам начислели");
-        System.out.println(x + " бонусных миль");
+        System.out.println((bonus) + " бонусных миль");
 
 
     }
